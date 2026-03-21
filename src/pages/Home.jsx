e63e3b1f45e943a247833f72e1cdae1e25b1306f
Piotr Dołgoszyja <157293235+specsxc@users.noreply.github.com>
2026-03-21 23:20:51 +0100

    Refactor (#1)
    * add router, refactor results
    
    * add pages and header
    
    * refactor fetching questions
    
    * simplify code

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="welcome-title">Quizzical</h1>
+      <h2 className="welcome-desc">Some description if needed</h2>
+    </div>
+  );
+}
